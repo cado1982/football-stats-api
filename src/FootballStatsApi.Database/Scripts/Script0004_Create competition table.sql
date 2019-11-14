88875df22f9eec8d64c59c7958fa5c2a6cf8abe8
Chris Oliver <cado82@gmail.com>
2019-11-14 14:46:19 +0000

    Add competitions table

---
+++ src/FootballStatsApi.Database/Scripts/Script0004_Create competition table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE "stats"."competition" (
+    "id" integer PRIMARY KEY,
+    "name" text NOT NULL,
+    "internal_name" text NOT NULL
+);
+
+INSERT INTO "stats"."competition" ("id", "name", "internal_name") VALUES
+    (1, 'English Premier League', 'EPL'),
+    (2, 'La Liga', 'La_liga'),
+    (3, 'Bundesliga', 'Bundesliga'),
+    (4, 'Serie A', 'Serie_A'),
+    (5, 'Ligue 1', 'Ligue_1'),
+    (6, 'Russian Premier League', 'RFPL');
+    
