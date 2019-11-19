d4875e1bc8a7f811c3f83ac3f074db58e2ebfac3
Chris Oliver <cado82@gmail.com>
2019-11-19 17:54:54 +0000

    Add fixture table

---
+++ src/FootballStatsApi.Database/Scripts/Script0007_Add fixture table.sql
@@ -0,0 +1,18 @@
+﻿CREATE SCHEMA IF NOT EXISTS "stats";
+
+CREATE TABLE "stats"."fixture" (
+    "fixture_id"                            integer PRIMARY KEY,
+    "home_team_id"                          integer NOT NULL REFERENCES "stats"."team"(id) ON DELETE CASCADE,
+    "away_team_id"                          integer NOT NULL REFERENCES "stats"."team"(id) ON DELETE CASCADE,
+    "season_id"                             integer NOT NULL,
+    "competition_id"                        integer NOT NULL REFERENCES "stats"."competition"(id) ON DELETE CASCADE,
+    "is_result"                             boolean NOT NULL,
+    "home_goals"                            integer NULL,
+    "away_goals"                            integer NULL,
+    "expected_home_goals"                   real NULL,
+    "expected_away_goals"                   real NULL,
+    "home_win_forecast"                     real NULL,
+    "home_draw_forecast"                    real NULL,
+    "home_loss_forecast"                    real NULL,
+    "datetime"                              timestamp NOT NULL
+);
