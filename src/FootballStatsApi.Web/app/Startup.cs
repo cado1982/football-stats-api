69b457e810d45b5d9bb89afdfe89cc747df359e1
Chris Oliver <cado82@gmail.com>
2019-12-03 14:23:21 +0000

    Add nginx to web project

--- src/FootballStatsApi.Web/app/Startup.cs
+++ src/FootballStatsApi.Web/app/Startup.cs
@@ -84,10 +84,7 @@ namespace FootballStatsApi.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
