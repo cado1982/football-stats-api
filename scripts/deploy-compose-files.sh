bd3d858f1ef93f7263afe825da06414f9d2bf328
Chris Oliver <cado82@gmail.com>
2019-12-10 15:28:47 +0000

    Combine web and api services onto one droplet

--- scripts/deploy-compose-files.sh
+++ scripts/deploy-compose-files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 scp ~/Documents/git/football-stats-api/src/docker-compose.production.footballstatsapi-1.yml chrisoliver@178.62.90.155:~/docker-compose.yml
-scp ~/Documents/git/football-stats-api/src/docker-compose.production.fsa-api-1.yml chrisoliver@134.209.191.244:~/docker-compose.yml
+scp ~/Documents/git/football-stats-api/src/docker-compose.production.fsa-api.fsa-web.yml chrisoliver@134.209.191.244:~/docker-compose.yml
 scp ~/Documents/git/football-stats-api/src/docker-compose.production.fsa-orchestration.yml chrisoliver@68.183.38.189:~/docker-compose.yml
-scp ~/Documents/git/football-stats-api/src/docker-compose.production.fsa-web-1.yml chrisoliver@134.209.191.129:~/docker-compose.yml
+scp ~/Documents/git/football-stats-api/src/docker-compose.production.fsa-api.fsa-web.yml chrisoliver@134.209.191.129:~/docker-compose.yml
 
 # chmod u+x ./scripts/deploy-compose-files.sh
