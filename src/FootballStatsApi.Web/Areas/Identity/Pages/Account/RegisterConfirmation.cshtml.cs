2d4dd279bb51dc1d123fb6b74ddd7bf65d856680
Chris Oliver <cado82@gmail.com>
2019-12-11 01:27:30 +0000

    Progress on email sender

--- src/FootballStatsApi.Web/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ src/FootballStatsApi.Web/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -29,32 +29,11 @@ namespace FootballStatsApi.Web.Areas.Identity.Pages.Account
 
         public string EmailConfirmationUrl { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(string email)
+        public IActionResult OnGet(string email)
         {
             if (email == null)
             {
                 return RedirectToPage("/Index");
-            }
-
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with email '{email}'.");
-            }
-
-            Email = email;
-            // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
-            if (DisplayConfirmAccountLink)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                EmailConfirmationUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code },
-                    protocol: Request.Scheme);
             }
 
             return Page();
--- src/FootballStatsApi.Web/Helpers/EmailSender.cs
+++ src/FootballStatsApi.Web/Helpers/EmailSender.cs
@@ -1,21 +1,43 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.Net.Mail;
+using FootballStatsApi.Web.Models;
 
 namespace FootballStatsApi.Web.Helpers
 {
     public class EmailSender : IEmailSender
     {
         private readonly ILogger<EmailSender> _logger;
+        private readonly SmtpConnectionInfo _smtpConnectionInfo;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public EmailSender(ILogger<EmailSender> logger)
+        public EmailSender(ILogger<EmailSender> logger, SmtpConnectionInfo smtpConnectionInfo, IWebHostEnvironment hostingEnvironment)
         {
-            this._logger = logger;
+            _logger = logger;
+            _smtpConnectionInfo = smtpConnectionInfo;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            this._logger.LogInformation($"Sending email to {email} with subject {subject} and body {htmlMessage}");
+            using (var client = new SmtpClient(_smtpConnectionInfo.Host, _smtpConnectionInfo.Port))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(_smtpConnectionInfo.Username, _smtpConnectionInfo.Password);
+                client.EnableSsl = true;
+
+                var message = new MailMessage(_smtpConnectionInfo.FromAddress, email);
+                message.IsBodyHtml = true;
+                message.Body = htmlMessage;
+                message.Subject = subject;
+
+                client.Send(message);
+            }
+            
             return Task.CompletedTask;
         }
     }
